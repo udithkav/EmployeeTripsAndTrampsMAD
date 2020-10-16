bf8008ebdceef1950d366ba2e731d6e8ff960d8e
Madhubala27 <mathumathan27@gmail.com>
2020-10-16 22:43:53 +0530

    Expenses Update,Delete,Search

--- app/src/main/java/com/example/employeetripsandtramps/expenses_details.java
+++ app/src/main/java/com/example/employeetripsandtramps/expenses_details.java
@@ -2,11 +2,26 @@ package com.example.employeetripsandtramps;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,9 +29,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class expenses_details extends Fragment {
+TextView searchExpenseID,userName,amount,details;
+Button update,delete,search;
+DatabaseReference dbRef;
+SearchExpenses expenses;
+Boolean viewCheck = false;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -28,14 +46,7 @@ public class expenses_details extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment expenses_details.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static expenses_details newInstance(String param1, String param2) {
         expenses_details fragment = new expenses_details();
@@ -60,5 +71,92 @@ public class expenses_details extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_expenses_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        searchExpenseID = (TextView)  getView().findViewById(R.id.editid);
+        userName = (TextView)  getView().findViewById(R.id.editname);
+        amount = (TextView)  getView().findViewById(R.id.editExpAmount);
+        details = (TextView)  getView().findViewById(R.id.editExpDetails);
+        update = (Button) getView().findViewById(R.id.updateExpButton);
+        delete = (Button) getView().findViewById(R.id.expDeleteButton);
+        search = (Button) getView().findViewById(R.id.searchExpenseDetails);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbRef = FirebaseDatabase.getInstance().getReference().child("Expense");
+                dbRef.child(searchExpenseID.getText().toString().trim());
+                viewCheck = false;
+            }
+        });
+
+
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbRef = FirebaseDatabase.getInstance().getReference().child("Expense").child(searchExpenseID.getText().toString().trim());
+                dbRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.exists()){
+                            expenses.setExpenseID(searchExpenseID.getText().toString().trim());
+                            expenses.setUsern(snapshot.child("usern").getValue().toString());
+                            expenses.setAmountt(snapshot.child("amountt").getValue().toString());
+                            expenses.setDetails(snapshot.child("details").getValue().toString());
+
+                            userName.setText(expenses.getUsern());
+                            amount.setText(expenses.getAmountt());
+                            details.setText(expenses.getDetails());
+                            viewCheck = true;
+                        }
+                        else{
+                            userName.setText("Does not Exist");
+                            amount.setText("Does not Exist");
+                            details.setText("Does not Exist");
+                            Toast.makeText(getActivity(),"Expense doesnt not Exist!",Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
+            }
+
+        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(viewCheck == true){
+                    final SearchExpenses e1 = null;
+                    e1.setUsern(userName.getText().toString().trim());
+                    e1.setDetails(details.getText().toString().trim());
+                    e1.setAmountt(amount.getText().toString().trim());
+                    e1.setExpenseID(searchExpenseID.getText().toString().trim());
+
+                    dbRef = FirebaseDatabase.getInstance().getReference().child("Expense");
+                    HashMap hashMap = new HashMap();
+                    hashMap.put("amountt",e1.getAmountt());
+                    hashMap.put("usern",e1.getUsern());
+                    hashMap.put("details",e1.getDetails());
+
+                    dbRef.child(String.valueOf(e1.getExpenseID())).updateChildren(hashMap);
+                    Toast.makeText(getActivity(), "Data Succesfully Updated", Toast.LENGTH_SHORT).show();
+                    viewCheck = false;
+                }
+                else{
+                    Toast.makeText(getActivity(),"Please Search for a Expense first!",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
     }
 }
